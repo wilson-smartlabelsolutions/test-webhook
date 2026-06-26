1bca8b9bde39a82e55f2cb7699546143eb05b470
Wilson Leung <wilson.leung@smartlabelsolutions.com>
2026-06-26 11:31:42 -0700

    debug

--- src/verifyShopifyWebhook.js
+++ src/verifyShopifyWebhook.js
@@ -10,8 +10,8 @@ function verifyShopifyWebhook(rawBody, hmacHeader, secret) {
     .update(rawBody)
     .digest("base64");
 
-  const digestBuffer = Buffer.from(digest, "utf8");
-  const hmacBuffer = Buffer.from(hmacHeader, "utf8");
+  const digestBuffer = Buffer.from(digest, "base64");
+  const hmacBuffer = Buffer.from(hmacHeader, "base64");
   console.log("digestBuffer: ", digestBuffer);
   console.log("hmacBuffer: ", hmacBuffer);
   if (digestBuffer.length !== hmacBuffer.length) {
